3945070e8679bda84e9c4012259f6c6360d4651d
kuian2202 <bernaldezkurtian@gmail.com>
2023-12-06 23:00:34 +0800

    Kurt - servicerequest

--- serviceyoufinal/servicerequest/urls.py
+++ serviceyoufinal/servicerequest/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('servicerequest/', views.ServiceRequest.as_view(), name='servicerequest'),
+    path('servicerequest/<int:request_id>/', views.ServiceRequest.as_view(), name='service_request_detail'),
 ]
