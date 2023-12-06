eacfabc029e280fb5752fbca560719d74764af3e
JasonCarloObiso <144089877+JasonObiso@users.noreply.github.com>
2023-12-06 17:55:56 +0800

    [Obiso] - Integrated old rateservice files to ServiceYOuFinal

--- serviceyoufinal/rateservice/models.py
+++ serviceyoufinal/rateservice/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from account.models import *
 
 # Create your models here.
--- serviceyoufinal/rateservice/views.py
+++ serviceyoufinal/rateservice/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from .form import RateServiceForm
+
 
 # Create your views here.
+
+def rating(request):
+    return HttpResponse("SUCCESS!")
+
+
+class RateService(View):
+    template = 'rating.html'
+
+    def get(self, request):
+        score = RateServiceForm()
+        return render(request, self.template, {'form': score})
