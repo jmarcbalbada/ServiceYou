ab7cbf000b63632795156c397190d6a1bbf7ed9b
Winson Baring <132115478+winsonbaring@users.noreply.github.com>
2023-12-17 23:19:23 +0800

    [Winson] - Current Progress 4

--- serviceyoufinal/acceptservice/urls.py
+++ serviceyoufinal/acceptservice/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('acceptservice/', views.acceptservice, name='acceptservice'),
+    path('pendingservice/', views.pendingservice, name='pendingservice'),
+    path('completedservice/', views.completedservice, name='completedservice'),
+    path('failedservice/', views.failedservice, name='failedservice'),
+    path('cancelledservice/', views.cancelledservice, name='cancelledservice'),
 ]
